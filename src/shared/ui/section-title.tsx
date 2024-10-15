45e04f6230333440dc50cc5b180ba137762a5cf3
Quetan <quetan@vk.com>
2024-10-15 17:02:41 +0700

    Add section title component

---
+++ src/shared/ui/section-title.tsx
@@ -0,0 +1,18 @@
+import { cn } from "../lib/utils";
+
+type SectionTitleProps = React.PropsWithChildren &
+	React.HTMLAttributes<HTMLHeadingElement> & {};
+
+const SectionTitle: React.FC<SectionTitleProps> = ({
+	children,
+	className,
+	...props
+}) => {
+	return (
+		<h1 className={cn("h1 mb-7 w-max mx-auto relative", className)} {...props}>
+			{children}
+		</h1>
+	);
+};
+
+export { SectionTitle };
