0d61972a821ca817d04e158bd048e79aaa187c3a
Quetan <quetan@vk.com>
2024-10-17 16:54:13 +0700

    Update payment methods mobile section

--- src/widgets/payment-methods/ui/payment-methods.tsx
+++ src/widgets/payment-methods/ui/payment-methods.tsx
@@ -9,14 +9,18 @@ import { Section } from "@/shared/ui/section";
 import { SectionTitle } from "@/shared/ui/section-title";
 import Autoplay from "embla-carousel-autoplay";
 import { paymentMethods } from "../api";
+import { useWindowDimensions } from "@/shared/lib/hooks";
+import { cn } from "@/shared/lib/utils";
 
 const PaymentMethods: React.FC = () => {
 	return (
-		<Section className="pt-[98px] pb-[89px] px-[135px]">
-			<SectionTitle className="mb-[46px]">Payment Methods</SectionTitle>
+		<Section className="pt-[54px] px-4 pb-[48px] lg:pt-[98px] lg:pb-[89px] lg:px-[135px]">
+			<SectionTitle className="mb-[34px] lg:mb-[46px]">
+				Payment Methods
+			</SectionTitle>
 
 			<Carousel
-				className="w-[1024px] mx-auto"
+				className="lg:w-[1024px] mx-auto"
 				opts={{
 					loop: true,
 					align: "center",
@@ -40,8 +44,14 @@ const PaymentMethods: React.FC = () => {
 const PlaymentMethodItem: React.FC<React.PropsWithChildren> = ({
 	children,
 }) => {
+	const { width } = useWindowDimensions();
 	return (
-		<CarouselItem className="basis-1/6 p-0 flex justify-center items-center">
+		<CarouselItem
+			className={cn(
+				"p-0 flex justify-center items-center",
+				width > 1024 ? "basis-1/6" : "basis-1/2"
+			)}
+		>
 			{children}
 		</CarouselItem>
 	);
